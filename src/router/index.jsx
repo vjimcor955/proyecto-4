7ce7636bc06986c4ab4a53f4b8a67163491525c5
vjimcor955 <vjimcor955@g.educaand.es>
2024-01-16 00:26:06 +0100

    Index actualizado

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,43 +1,26 @@
 import React from 'react'
-import { createBrowserRouter } from "react-router-dom"
-import Home from "../pages/Home"
-import User from "../pages/User"
-import Search from '../pages/Search'
-import LoginPage from '../pages/LoginPage'
-import LayoutPrivate from "../layouts/LayoutPrivate"
+import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
+// Layouts
 import LayoutRoot from "../layouts/LayoutRoot"
+import LayoutPrivate from "../layouts/LayoutPrivate"
+// Pages
+import Home from "../pages/Home"
+import Search from '../pages/Search'
+import Contact from '../pages/Contact'
+import LoginPage from '../pages/LoginPage'
+import User from "../pages/User"
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LayoutRoot />,
-    children: [
-      {
-        index: true,
-        element: <Home />
-      },
-      {
-        path: "/search",
-        element: <Search />
-      },
-      // {
-      //   path: "/search/:id",
-      //   element: <Contact />
-      // },
-      {
-        path: "/login",
-        element: <LoginPage />
-      },
-      {
-        path: "/dashboard",
-        element: <LayoutPrivate />,
-        children: [
-          {
-            index: true,
-            element: <User />,
-          },
-        ]
-      }
-    ]
-  }
-])
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<LayoutRoot />}>
+      <Route index element={<Home />} />
+      <Route path='/search' element={<Search />} />
+      <Route path='/pokemon/:id' element={<Contact />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/user' element={<LayoutPrivate />}>
+        <Route index element={<User />} />
+      </Route>
+    </Route>
+  )
+)
