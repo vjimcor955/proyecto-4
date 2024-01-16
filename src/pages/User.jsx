cf8d51ffdab985796a82a99fdf833ff250c941a9
vjimcor955 <vjimcor955@g.educaand.es>
2024-01-16 23:22:24 +0100

    Pagina user funcional

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useUserContext } from '../context/UserContext'
 
 const User = () => {
+  const {getUserData, email} = useUserContext()
+  const userData = getUserData(email)
+
   return (
-    <div>User</div>
+    <div className='user'>
+      <h1 className='user__title'>Datos Usuario</h1>
+      <div className="user__data">
+        <ul>
+          <li>Nombre: {userData.name}</li>
+          <li>Email: {userData.email}</li>
+          <li>Fecha de nacimiento: {userData.date}</li>
+          <li>Contraseña: {userData.password}</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
