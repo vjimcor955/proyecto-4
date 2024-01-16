84c94de4c71bad9fcf38de9df08c51246ef732ab
vjimcor955 <vjimcor955@g.educaand.es>
2024-01-16 20:46:36 +0100

    Home page terminada

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div className='home'>
-      Home
+      <div className="home__content">
+        <h1 className='home__content--title'>PokeAPI</h1>
+        <p className='home__content--messagge'>Buscador de pokemons online</p>
+        <button className='home__content--button'><NavLink to="/search" className='link'>Buscar Pokemons</NavLink></button>
+      </div>
     </div>
   )
 }
