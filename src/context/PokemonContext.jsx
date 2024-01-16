9547e70699f8ae5cd14b148cb4103f9c750558ed
vjimcor955 <vjimcor955@g.educaand.es>
2024-01-16 19:27:47 +0100

    Refactorizacion y renombre PokemonContext

--- src/context/PokemonContext.jsx
+++ src/context/PokemonContext.jsx
@@ -1,5 +1,4 @@
 import { createContext, useState, useContext, useEffect } from "react";
-import { useForms } from "../hooks/useForms";
 
 export const PokemonContext = createContext()
 
@@ -10,29 +9,7 @@ const PokemonProvider = ({children}) => {
   const [allPokemonsList, setAllPokemonsList] = useState([])
   const [versions, setVersions] = useState([])
   const [types, setTypes] = useState([])
-  const [loading, setLoading] = useState(true)
 
-  const {valueSearch, handleInputChange, resetForm} = useForms({
-    valueSearch: ""
-  })
-
-  // Get a list of pokemons
-  // limit: number og pokemons in the list
-  // offset: index of the pokemon the list begins
-  const getPokemons = async (limit = 20) => {
-    const url = "https://pokeapi.co/api/v2"
-    const res = await fetch(`${url}/pokemon?limit=${limit}&offset=${offset}`)
-    const data = await res.json()  
-    // Promise to get the data of each pokemon
-    const promise = data.results.map(async(pokemon) => {
-      const res = await fetch(pokemon.url)
-      const data = await res.json()
-      return data
-    })
-    const resultados = await Promise.all(promise)
-    setPokemonList([...pokemonList, ...resultados])
-    setLoading(false)
-  }
 
   // Get a list contaning all pokemons (filter-oriented)
   const getAllPokemons = async () => {
@@ -47,7 +24,6 @@ const PokemonProvider = ({children}) => {
     })
     const resultados = await Promise.all(promise)
     setAllPokemonsList(resultados)
-    setLoading(false)
   }
 
   // Get a pokemon data by Id
@@ -58,14 +34,6 @@ const PokemonProvider = ({children}) => {
     return data
   }
 
-  // Get all game versions
-  const getAllVersions = async () => {
-    const url = "https://pokeapi.co/api/v2/version"
-    const res = await fetch(url)
-    const data = await res.json()  
-    setVersions(data.results)
-  }
-
   // Get all pokemon types
   const getAllTypes = async () => {
     const res = await fetch("https://pokeapi.co/api/v2/type")
@@ -74,18 +42,13 @@ const PokemonProvider = ({children}) => {
   }
 
   useEffect(() => {
-    getPokemons()
-    getAllVersions()
+    getAllPokemons()
     getAllTypes()
   }, [])
 
-  useEffect(() => {
-    // getAllPokemons()
-  })
-
 
   return (
-    <PokemonContext.Provider value={{versions, types, pokemonList, allPokemonsList, getPokemonById, valueSearch, handleInputChange, resetForm}}>
+    <PokemonContext.Provider value={{versions, types, pokemonList, allPokemonsList, getPokemonById}}>
       {children}
     </PokemonContext.Provider>
   )
