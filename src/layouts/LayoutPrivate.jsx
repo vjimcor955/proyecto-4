d8cb350335d6ef7677cb7cf2acab6ec24138a0a4
vjimcor955 <vjimcor955@g.educaand.es>
2024-01-16 22:55:34 +0100

    Refactorizacion LayoutPrivate

--- src/layouts/LayoutPrivate.jsx
+++ src/layouts/LayoutPrivate.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { useUserContext } from '../context/UserContext'
 import { Navigate } from 'react-router-dom'
